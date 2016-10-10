ee0125ff727a656a2892ff483b1f2ca282be530f
Diana Frank <diana.frank22@gmail.com>
2016-10-10 15:38:09 -0400

    changes inputbox

--- src/components/CPUBoard.js
+++ src/components/CPUBoard.js
@@ -1,5 +1,6 @@
 import React, { PropTypes, Component } from 'react';
 import {Link} from 'react-router';
+import InputBox from './InputBox'
 
 export default class CPUBoard extends React.Component {
   constructor(props){
@@ -10,27 +11,23 @@ export default class CPUBoard extends React.Component {
   }
 
   selectCoordinate(space, e){
-    //
-    console.log(this.state.selectedCoordinate)
-    console.log(e.target.value)
     if(this.state.selectedCoordinate !== undefined){
       var remove = this.state.selectedCoordinate
-      var r = document.getElementById(remove)
-      r.classList.remove('selected')
-      addClass(space, e)
+      var r = document.getElementById('cpu-space'+remove)
+      r.classList.remove('selectedCoordinate')
+      this.addClass(space, e)
+    }else{
+      this.addClass(space, e)
     }
-    // update selected space here
-    // can only have one value at a time, updating value of that with background?
-    // if this.state.selectedCoordinate !== undefined
-    // remove class of that
-    // add class to e
-    // and update class of that one
   }
 
 addClass(space,e){
   var id = e.target.id
   var el = document.getElementById(id)
   el.classList.add('selectedCoordinate')
+  this.setState({
+    selectedCoordinate: space
+  })
 }
 
 
@@ -44,7 +41,7 @@ addClass(space,e){
       for(let j=0; j< spaces.length; j++){
         var space = spaces[j]
         let clickHandler = this.selectCoordinate.bind(this, space)
-        spaceHtml = <div id={'cpu-space'+{space}} className="cpu space" key={space} value={space} onClick={clickHandler}> {space} </div>
+        spaceHtml = <div id={'cpu-space'+space} className="cpu space" key={space} value={space} onClick={clickHandler}> {space} </div>
         htmlArray.push(spaceHtml)
       }
     }
--- src/components/InputBox.js
+++ src/components/InputBox.js
@@ -4,21 +4,27 @@ import ReactDOM from 'react-dom';
 export default class InputBox extends React.Component{
   constructor(props){
     super();
-    this.state ={}
+    this.state ={
+      previouslySelected: [],
+      hitSpaces: []
+    }
+    this.handleChange = this.handleChange.bind(this);
   }
 
   // selected space will be updated in selection component,
   // can't select depending on state
-  //       previouslySelected: [],
-        // hitSpaces: []
 // this.props.selectedCoordinate as input value
 // either display button to submit all coordinates based state
 // or input box to select cordinate on cpu
-// or text that says wait for cpu selection 
+// or text that says wait for cpu selection
+
 
   render(){
     return(
-
+      <input
+              type="text"
+              value={this.props.selectedCoordinate}
+            />
     )
 
   }
