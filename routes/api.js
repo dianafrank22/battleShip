3de6015f19022210a131462a0ee230aa99f09401
Diana Frank <diana.frank22@gmail.com>
2016-10-11 15:26:37 -0400

    changes api.js

--- routes/api.js
+++ routes/api.js
@@ -1,28 +1,28 @@
 const router       = require('express').Router();
-const { createGame, endGame, setPlayerShips, createCpuCoordinates, setCpuCoorindates, sendPlayersMove, checkSuccess,returnSuccessStatus, getCPUMove }  = require('../api/game_controls')
+const game = require('../api/game_controls')
 
 // @TODO
 // check for game in session first?
 
 // create a game
-router.get('/start', createGame, (req, res) => {
+router.get('/start', game.createGame, (req, res) => {
   res.status(200).json({response: res.game});
 })
 
 
 // update players coordinates
-router.put('/setShips', setPlayerShips, createCpuCoordinates, (req, res) => {
+router.put('/setShips', game.setPlayerShips, game.createCpuCoordinates, (req, res) => {
   res.status(200).json({response: res.cpuCoordinates})
 })
 
 // end game
-router.delete('/end', endGame, (req, res) => {
+router.delete('/end', game.endGame, (req, res) => {
   console.log(res)
   res.status(200).json({response: res.game})
 })
 
 // // Accepts coordinates for the player’s next move.
-router.post('/missile', sendPlayersMove, getCPUMove, (req, res) =>{
+router.post('/missile', game.sendPlayersMove, game.getCPUMove, (req, res) =>{
   res.json({response: res.cpuSelectedCoordinate})
 })
 
